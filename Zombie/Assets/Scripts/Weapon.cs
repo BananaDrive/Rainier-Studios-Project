2619ec6b8e2e6d468d97a0f6caf711c96ffe95e0
4Forest1 <runareana1001@gmail.com>
2025-02-27 12:42:38 -0800

    Bullet and able to kill enemies now

--- Zombie/Assets/Scripts/Weapon.cs
+++ Zombie/Assets/Scripts/Weapon.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
 using NUnit.Framework;
 using Unity.Burst;
 using UnityEngine;
@@ -11,34 +13,60 @@ public class Weapon : MonoBehaviour
     public float fireRate;
     public float clipSize;
     public float bulletSpeed;
+    public bool isRaycast;
 
     bool shootCooldown;
-    void Start()
-    {
-
-    }
+    public LayerMask playerLayer;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !shootCooldown) 
+        if (Input.GetKeyDown(KeyCode.E) && !shootCooldown)
         {
-            shootCooldown = true;
-            StartCoroutine(Shoot()); //placeholder for shooting, will improve tmr
+            if (isRaycast)
+            {
+                shootCooldown = true;
+                RaycastShoot();
+            }
+            else
+            {
+                shootCooldown = true;
+                ProjectileShoot();
+            }
         } 
     }
 
-    public IEnumerator Shoot()
+    public void ProjectileShoot()
     {
         GameObject bullet = ObjectPool.SharedInstance.GetPooledObject();
+
+        if (bullet == null)
+            return;
+
+        bullet.SetActive(true);
         
-        if (bullet != null)
+        bullet.GetComponent<Transform>().position = transform.position;
+        bullet.GetComponent<Bullet>().damage = damage;
+        StartCoroutine(bullet.GetComponent<Bullet>().Despawn());
+        bullet.GetComponent<Rigidbody2D>().AddForce(bulletSpeed * 10f * transform.right, ForceMode2D.Force);
+
+        StartCoroutine(ShootCD());
+    }
+
+    public void RaycastShoot()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 30f, ~playerLayer);
+
+        if (hit.collider != null)
         {
-            bullet.SetActive(true);
-            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
-            bullet.GetComponent<Transform>().position = transform.position;
-            bulletRb.AddForce(bulletSpeed * 10f * transform.right, ForceMode2D.Force);
-            yield return new WaitForSeconds(fireRate);
-            shootCooldown = false;
+            if (hit.transform.TryGetComponent(out Health hitHealth))
+                hitHealth.TakeDamage(damage);
         }
+        StartCoroutine(ShootCD());
+    }
+
+    public IEnumerator ShootCD()
+    {
+        yield return new WaitForSeconds(fireRate);
+        shootCooldown = false;
     }
 }

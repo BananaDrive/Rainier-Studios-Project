c0f5bfbbf277a60c368df9c5172c938e42a42673
4Forest1 <runareana1001@gmail.com>
2025-04-04 12:59:49 -0700

    how to add animation parameters

---
+++ Zombie/Assets/Script/Enviroment/EnviromentCollision.cs
@@ -0,0 +1,17 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class EnviromentCollision : MonoBehaviour
+{
+    public LayerMask layersToTrigger;
+    public Animator anim;
+    public AnimationClip animClip;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if ((layersToTrigger & (1 << other.gameObject.layer)) != 0)
+        {
+            anim.Play(animClip.ToString());
+        }
+    }
+}

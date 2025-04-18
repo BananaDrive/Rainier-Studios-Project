30c7af0e15c171726d4154f71c24cdfe1b77e30e
Ripper4319 <filmguy4319@gmail.com>
2025-04-18 13:34:55 -0700

    arms fixed bewtter now

--- Zombie/Assets/Script/Player/Aiming.cs
+++ Zombie/Assets/Script/Player/Aiming.cs
@@ -15,7 +15,7 @@ public class Aiming : MonoBehaviour
         float aimDirection = Input.GetAxisRaw("Vertical");
 
         weapon.rotation = Quaternion.Euler(weapon.rotation.x, movement.transform.eulerAngles.y, aimDirection * 90); //determines the weapon direction from the player direction and its vertical input
-        Vector2 rotationDistance = new(offsetX * aimSide * Mathf.Cos(aimDirection * Mathf.PI / 2), offsetY * (0.3f * Mathf.Sin(aimDirection * Mathf.PI / 2))); //makes the weapon seem like its orbiting the player
-        weapon.position = new(transform.position.x + rotationDistance.x + offsetOriginX, transform.position.y + rotationDistance.y + offsetOriginY);
+        Vector2 rotationDistance = new(offsetX * Mathf.Cos(aimDirection * Mathf.PI / 2), offsetY * (0.3f * Mathf.Sin(aimDirection * Mathf.PI / 2))); //makes the weapon seem like its orbiting the player
+        weapon.position = new(transform.position.x + rotationDistance.x + (offsetOriginX * aimSide), transform.position.y + rotationDistance.y + offsetOriginY);
     }
 }  

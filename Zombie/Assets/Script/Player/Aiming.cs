090d2b7249455534e587d659c97f44f934674097
Ripper4319 <filmguy4319@gmail.com>
2025-04-18 13:06:46 -0700

    Need arm Updated. also TUT Level sstarated

--- Zombie/Assets/Script/Player/Aiming.cs
+++ Zombie/Assets/Script/Player/Aiming.cs
@@ -5,6 +5,8 @@ public class Aiming : MonoBehaviour
     public Transform weapon;
     public Movement movement;
 
+    public float offsetX, offsetY;
+
     // Update is called once per frame
     void Update()
     {
@@ -12,7 +14,7 @@ public class Aiming : MonoBehaviour
         float aimDirection = Input.GetAxisRaw("Vertical");
 
         weapon.rotation = Quaternion.Euler(weapon.rotation.x, movement.transform.eulerAngles.y, aimDirection * 90); //determines the weapon direction from the player direction and its vertical input
-        Vector2 rotationDistance = new(0.1f * aimSide * Mathf.Cos(aimDirection * Mathf.PI / 2), 0.34f + (0.3f * Mathf.Sin(aimDirection * Mathf.PI / 2))); //makes the weapon seem like its orbiting the player
+        Vector2 rotationDistance = new(offsetX * aimSide * Mathf.Cos(aimDirection * Mathf.PI / 2), offsetY + (0.3f * Mathf.Sin(aimDirection * Mathf.PI / 2))); //makes the weapon seem like its orbiting the player
         weapon.position = new(transform.position.x + rotationDistance.x, transform.position.y + rotationDistance.y);
     }
 }

adf856dac14ad1e4d685384744939b2d27e2c5bd
4Forest1 <runareana1001@gmail.com>
2025-05-07 13:27:06 -0700

    LeaderBoard almost added

--- Zombie/Assets/Script/Game Manager.cs
+++ Zombie/Assets/Script/Game Manager.cs
@@ -8,10 +8,13 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverScreen;
     public GameObject pauseScreen;
     public LootTable lootTable;
+    public BuffsHandler buffsHandler;
+    public NameInput nameInput;
+    public Leaderboard leaderboard;
     public bool isPaused, gameOver;
     void Awake()
     {
-        if (Instance != null && Instance != this)
+        if ((Instance != null && Instance != this) || SceneManager.GetActiveScene().buildIndex == 0)
             Destroy(this);
         else
             Instance = this;
@@ -36,6 +39,14 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1;
         gameOverScreen.SetActive(false);
+
+        ScoreEntry scoreEntry = new()
+        {
+            score = UIManager.score,
+            playerName = nameInput.GetName()
+        };
+
+        Debug.Log(scoreEntry.playerName + " " + scoreEntry.score);
         SceneManager.LoadScene(0);
     }
 

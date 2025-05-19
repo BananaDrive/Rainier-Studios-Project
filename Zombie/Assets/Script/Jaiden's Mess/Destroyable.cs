0eeec3fbce54b0c9aa778a5482f132798bf7f173
Jaiden <Jmanfrohlich@gmail.com>
2025-05-19 13:40:53 -0700

    redesign

---
+++ Zombie/Assets/Script/Jaiden's Mess/Destroyable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour
+{
+    public int health = 5;
+    public int maxHealth = 5;
+    public int damageGiven = 1;
+    public int damageRecieved = 1;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlayerBullets")
+        {
+            health -= damageRecieved;
+            Destroy(collision.gameObject);
+        }
+    }
+}
